3026e208938c90c251e59a0ba13b6c6a2c0e1eb3
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2019-10-28 23:44:32 +0300

    1) добавление музыки на фон + попытка функции для изменения проигрываемого трека при определённых условиях 2) добавление музыкальных треков в папку проекта 3) изменение многоочерёдного условия с "или" на "any" [не работает]
    to do:
    - разобраться с "any"
    - добавить кнопки "кормить" и тд
    - ввод имени енота в самом начале

--- first.py
+++ first.py
@@ -6,6 +6,8 @@ from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QProgressBar, QMa
 from PyQt5.QtWidgets import QLCDNumber, QLineEdit
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
+from PyQt5.QtCore import *
+from PyQt5.QtMultimedia import *
 
 
 class MyWidget(QMainWindow):
@@ -13,8 +15,18 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main1.ui', self)
 
-        # self.pushButton.clicked.connect(self.run)
+        # проигрывание музыки на фоне
 
+        self.playlist = QMediaPlaylist()
+        self.url = QUrl.fromLocalFile('основа.mp3')
+        self.playlist.addMedia(QMediaContent(self.url))
+        self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+
+        self.player = QMediaPlayer()
+        self.player.setPlaylist(self.playlist)
+        self.player.play()
+
+        # основное изображение
         self.name_picture = 'сидит.jpg'
         self.picture1 = QPixmap(self.name_picture)
         self.picture.setPixmap(self.picture1)
@@ -33,14 +45,26 @@ class MyWidget(QMainWindow):
         self.step_mood = 100
         self.step_clean = 100
 
+        self.steps = [self.step_food, self.step_healthy, self.step_sleep,
+                      self.step_mood, self.step_clean]
+
         self.number_food = 0.2
         self.number_mood = 0.1
         self.number_sleep = 0.2
         self.number_clean = 0.3
         self.number_healthy = 0.1
 
+        # зависимость показателей друг от друга
+
         if self.step_food < 70:
             self.number_mood += 0.2
+
+        # тревожная музыка на фон
+        if any(steps < 50 for steps in self.steps):
+            self.url = QUrl.fromLocalFile('тревога.mp3')
+            self.playlist.addMedia(QMediaContent(self.url))
+            self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+        # зависимость главного изображения от показателей
 
     def timerEvent(self, e):
 
@@ -48,8 +72,7 @@ class MyWidget(QMainWindow):
 
         # случай, когда один из показателей равен нулю
 
-        if self.step_food < 1 or self.step_mood < 1 or self.step_clean < 1 \
-                or self.step_healthy < 1 or self.step_sleep < 1:
+        if any(steps < 1 for steps in self.steps):
             self.timer.stop()
             self.begin.setText('ВСЁ СНАЧАЛА')
 
@@ -58,6 +81,8 @@ class MyWidget(QMainWindow):
             self.picture.setPixmap(self.picture1)
             QApplication.processEvents()
             return
+
+        # изменение показателей прогресс-баров
 
         self.step_food = self.step_food - self.number_food
         self.food.setValue(self.step_food)
