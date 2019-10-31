27591e97b68452fe39cb7c9cbda23992410e8fbc
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2019-10-31 19:45:28 +0300

    1) добавление музыки на фон + попытка функции для изменения проигрываемого трека при определённых условиях 2) добавление музыкальных треков в папку проекта 3) функция для замены главного изображения (неправильно работает) 4) работа кнопок "кормить", "лечить" и т.д. 5) зависимость показателей друг от друга 6) класс Диалогового окна 7) работа кнопки для выдачи информации о музыке

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ import sys
 import time
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QProgressBar, QMainWindow
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QProgressBar, QMainWindow, QDialog
 from PyQt5.QtWidgets import QLCDNumber, QLineEdit
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
@@ -11,6 +11,8 @@ from PyQt5.QtMultimedia import *
 
 
 class MyWidget(QMainWindow):
+    """ главное окно"""
+
     def __init__(self):
         super().__init__()
         uic.loadUi('main1.ui', self)
@@ -45,11 +47,13 @@ class MyWidget(QMainWindow):
         self.step_mood = 100
         self.step_clean = 100
 
-        self.steps = [self.step_food, self.step_healthy, self.step_sleep,
-                      self.step_mood, self.step_clean]
+        self.steps = [int(self.step_food), int(self.step_healthy), int(self.step_sleep),
+                      int(self.step_mood), int(self.step_clean)]
 
-        self.number_food = 0.2
-        self.number_mood = 0.1
+        # изменение показателей
+
+        self.number_food = 0.25
+        self.number_mood = 0.15
         self.number_sleep = 0.2
         self.number_clean = 0.3
         self.number_healthy = 0.1
@@ -58,13 +62,39 @@ class MyWidget(QMainWindow):
 
         if self.step_food < 70:
             self.number_mood += 0.2
+        if self.step_mood < 60:
+            self.number_healthy += 0.2
+        if self.step_clean < 50:
+            self.number_healthy += 0.5
+        if self.step_healthy < 30:
+            self.number_healthy += 0.5
+        if self.step_healthy < 60:
+            self.number_food -= 0.2
+        if self.step_sleep < 50:
+            self.number_mood += 0.3
+        if self.step_mood < 40:
+            self.number_sleep += 0.2
 
-        # тревожная музыка на фон
-        if any(steps < 50 for steps in self.steps):
-            self.url = QUrl.fromLocalFile('тревога.mp3')
-            self.playlist.addMedia(QMediaContent(self.url))
-            self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
-        # зависимость главного изображения от показателей
+        # работа кнопок "кормить", "лечить" и тд
+
+        self.healthy_btn.clicked.connect(lambda: self.life('healthy', 15))
+        self.food_btn.clicked.connect(lambda: self.life('food', 20))
+        self.mood_btn.clicked.connect(lambda: self.life('mood', 25))
+        self.sleep_btn.clicked.connect(lambda: self.life('sleep', 40))
+        self.clean_btn.clicked.connect(lambda: self.life('clean', 50))
+
+        # работа кнопки про музыку
+
+        self.misic.clicked.connect(lambda: self.openDialog('музыка'))
+
+    def openDialog(self, name):
+
+        """ открытие диалогового окна"""
+
+        dialog = Information()
+        dialog.setWindowTitle(name)
+        dialog.setTextOnLabel(name)
+        dialog.exec_()
 
     def timerEvent(self, e):
 
@@ -72,15 +102,39 @@ class MyWidget(QMainWindow):
 
         # случай, когда один из показателей равен нулю
 
-        if any(steps < 1 for steps in self.steps):
+        if self.step_food < 1 or self.step_mood < 1 or self.step_clean < 1 \
+                or self.step_healthy < 1 or self.step_sleep < 1:
             self.timer.stop()
             self.begin.setText('ВСЁ СНАЧАЛА')
 
-            self.name_picture = 'обиделся.jpg'
-            self.picture1 = QPixmap(self.name_picture)
-            self.picture.setPixmap(self.picture1)
-            QApplication.processEvents()
+            self.picturePutOn('обиделся.jpg')
             return
+
+        # тревожная музыка на фон
+
+        if self.step_food < 50 or self.step_mood < 50 or self.step_clean < 50 \
+                or self.step_healthy < 50 or self.step_sleep < 50:
+            self.url = QUrl.fromLocalFile('тревога.mp3')
+            self.playlist.addMedia(QMediaContent(self.url))
+            self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+
+        # зависимость главного изображения от показателей
+
+        if self.step_food < 90:
+            self.picturePutOn('просит кушать.jpg')
+            QApplication.processEvents()
+        if self.step_mood < 90:
+            self.picturePutOn('скучно.jpg')
+            QApplication.processEvents()
+        if self.step_sleep < 90:
+            self.picturePutOn('хочет спать.jpg')
+            QApplication.processEvents()
+        if self.step_clean < 90:
+            self.picturePutOn('грязный.jpg')
+            QApplication.processEvents()
+        if self.step_healthy < 90:
+            self.picturePutOn('болеет.jpg')
+            QApplication.processEvents()
 
         # изменение показателей прогресс-баров
 
@@ -98,6 +152,45 @@ class MyWidget(QMainWindow):
 
         self.step_mood = self.step_mood - self.number_mood
         self.mood.setValue(self.step_mood)
+
+    def picturePutOn(self, name):
+
+        """ функция для замены основной картинки"""
+
+        self.name_picture = name
+        self.picture1 = QPixmap(self.name_picture)
+        self.picture.setPixmap(self.picture1)
+
+    def life(self, name, number):
+
+        """ увеличение показателей"""
+        self.number = number
+        self.name = name
+        if self.name == 'healthy':
+            if self.step_healthy + self.number < 100:
+                self.step_healthy += self.number
+            else:
+                self.step_healthy = 100
+        elif self.name == 'food':
+            if self.step_food + self.number < 100:
+                self.step_food += self.number
+            else:
+                self.step_food = 100
+        elif self.name == 'mood':
+            if self.step_mood + self.number < 100:
+                self.step_mood += self.number
+            else:
+                self.step_mood = 100
+        elif self.name == 'clean':
+            if self.step_clean + self.number < 100:
+                self.step_clean += self.number
+            else:
+                self.step_clean = 100
+        elif self.name == 'sleep':
+            if self.step_sleep + self.number < 100:
+                self.step_sleep += self.number
+            else:
+                self.step_sleep = 100
 
     def doAction(self):
 
@@ -119,6 +212,21 @@ class MyWidget(QMainWindow):
             self.timer.start(100, self)
             self.begin.setText('ЗАВЕРШИТЬ')
             QApplication.processEvents()
+
+
+class Information(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('information.ui', self)
+
+    def setTextOnLabel(self, name):
+        self.name = name
+        if self.name == 'музыка':
+            self.text.setText('Ти́моти Макке́нзи, более известный по сценическому имени Labrinth, — британский '
+                              'автор-исполнитель и музыкальный продюсер. В данном приложении используются'
+                              'его треки New Girl и Following Tyler')
+        self.text.setWordWrap(True)
+        QApplication.processEvents()
 
 
 if __name__ == '__main__':
